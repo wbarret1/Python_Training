fbcfa08b845a7cbfaf09f5043f8796462d1e11a0
wbarret1 <barrett.williamm@epa.gov>
2020-05-12 13:31:53 -0400

    Initial commit

---
+++ quicksort.py
@@ -0,0 +1,38 @@
+# Quicksort implementations from https://stackoverflow.com/questions/18262306/quicksort-with-python
+
+def qsort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        return qsort([x for x in arr[1:] if x < arr[0]]) + \
+               [arr[0]] + \
+               qsort([x for x in arr[1:] if x >= arr[0]])
+
+
+def partition(array, begin, end):
+    pivot = begin
+    for i in xrange(begin+1, end+1):
+        if array[i] <= array[begin]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+    array[pivot], array[begin] = array[begin], array[pivot]
+    return pivot
+
+
+
+def quicksort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+    def _quicksort(array, begin, end):
+        if begin >= end:
+            return
+        pivot = partition(array, begin, end)
+        _quicksort(array, begin, pivot-1)
+        _quicksort(array, pivot+1, end)
+    return _quicksort(array, begin, end)
+
+
+array = [97, 200, 100, 101, 211, 107]
+result_1 = qsort(array)
+quicksort(array)
+# array -> [97, 100, 101, 107, 200, 211]
